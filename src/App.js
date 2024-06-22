e9b897fcb2da4362498f6df82b360a9957a9244b
Uday Pratap Singh <udaypratapsingh95802@gmail.com>
2024-06-22 17:34:17 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Slider from "react-slick";
+import dummyData from "./Data";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+import { device } from './styles/Media'; // Assuming you have a Media.js file with breakpoints
 
-function App() {
+const App = () => {
+  const [change, setChange] = useState(0);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    beforeChange: (oldIndex, newIndex) => {
+      setChange(change + 1);
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Heading style={{ transform: `rotate(${change * 90}deg)` }}>Assignment</Heading>
+      <StyledSlider {...settings}>
+        {dummyData.map((item, index) => (
+          <div key={index}>
+            <ImageContainer>
+              <Image src={item.src} alt={item.Name} />
+            </ImageContainer>
+            <TextContainer>
+              <Title>{item.Name}</Title>
+              <Description>{item.description}</Description>
+            </TextContainer>
+          </div>
+        ))}
+      </StyledSlider>
+    </Container>
   );
-}
+};
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 50px;
+`;
+
+const Heading = styled.div`
+  color: white;
+  text-align: center;
+  font-size: 32px;
+  margin: 80px 0;
+`;
+
+const StyledSlider = styled(Slider)`
+  margin-left: 5vh;
+  margin-right: 5vh;
+  padding-left: 50px;
+  padding-right: 50px;
+
+  @media ${device.tablet} {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  @media ${device.mobileL} {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 30vh;
+  height: 30vh;
+  border: 0.5rem solid white;
+  object-fit: cover;
+
+  @media ${device.tablet} {
+    width: 25vh;
+    height: 25vh;
+  }
+
+  @media ${device.mobileL} {
+    width: 20vh;
+    height: 20vh;
+  }
+`;
+
+const TextContainer = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h3`
+  color: white;
+`;
+
+const Description = styled.p`
+  color: white;
+`;
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,57 @@
+import {house1,house2,house3,house4,house5,house6,house7,house8,house9,house10} from './assests'
+
+const dummyData = [
+    {
+      src: house1,
+      Name:'House 1',
+      description: 'A beautiful two-story house with a spacious garden and modern amenities.',
+    },
+    {
+      src: house2,
+      Name:'House 2',
+      description: 'A cozy apartment located in the heart of the city, perfect for urban living.',
+    },
+    {
+      src:house3,
+      Name:'House 3',
+      description: 'A luxurious mansion with a swimming pool and a large backyard.',
+    },
+    {
+      src: house4,
+      Name:'House 4',
+      description: 'A charming cottage in the countryside, surrounded by nature.',
+    },
+    {
+      src: house5,
+      Name:'House 5',
+      description: 'A contemporary villa with stunning architecture and sea views.',
+    },
+    {
+      src: house6,
+      Name:'House 6',
+      description: 'A stylish loft with an open floor plan and modern furnishings.',
+    },
+    {
+      src: house7,
+      Name:'House 7',
+      description: 'A beachfront property with direct access to the ocean and a private deck.',
+    },
+    {
+      src: house8,
+      Name:'House 8',
+      description: 'A historic townhouse with classic details and a cozy atmosphere.',
+    },
+    {
+      src: house9,
+      Name:'House 9',
+      description: 'A family-friendly home with multiple bedrooms and a large play area.',
+    },
+    {
+      src: house10,
+      Name:'House 10',
+      description: 'A sleek penthouse with panoramic city views and high-end finishes.',
+    },
+  ];
+  
+  export default dummyData;
+  
